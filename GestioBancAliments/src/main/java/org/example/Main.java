e40ec92f9b09dfe857bdac15350d94dd3da80c61
joanchorto1 <joanchorto1@iesebre.com>
2023-01-11 09:28:30 +0100

    push

---
+++ GestioBancAliments/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int menu() {
+        System.out.println("fdshdsgfdg");
+        Scanner ent = new Scanner(System.in);
+        int index= ent.nextInt();
+        return index;
+    }
+    public static int seleccionemAccio() {
+        switch (menu()){
+            case 1:
+                //Cridem la funció
+        }
+    }
+
+
+
+
+
+
+
+}
